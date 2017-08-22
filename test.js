c1d6542b3563418b90b20239560b3a6b2ff20bcc
Nishadhka <nishadhka@gmail.com>
2017-08-22 10:22:00 +0530

    The table is workin gon click, but the calcualtion is not happening
    it sayingthe outputbox1a is not defined, the testing of forms and tables
    formed by js now

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-function createPm10table() {
+function createTablePm10() {
     
 var myTable= "<table style='padding-bottom:5px'>";
 myTable+="<tr>"
@@ -143,4 +143,273 @@ myTable+="  </tr>"
 myTable+="  </tbody>"
 myTable+="  </table>"
 document.getElementById('tablePrint1').innerHTML = myTable;
+
+console.log("created the table");
+}
+
+function AQIPM10IND(Concentration) 
+ { 
+ var Conc=parseFloat(Concentration); 
+ var c; 
+ var AQI; 
+ c=Math.floor(Conc);           
+ if (c>=99.0 && c<0.0) 
+ { 
+ AQI=Linear(100.0,0.0,99.0,0.0,c); 
+ } 
+else if (c>=100.0 && c<149.0) 
+ { 
+ AQI=Linear(150.0,101.0,149.0,100.0,c); 
+ } 
+ else if (c>=150.0 && c<349.0) 
+ { 
+ AQI=Linear(350.0,151.0,349.0,150.0,c); 
+ } 
+ else if (c>=350.0 && c<419.0) 
+ { 
+ AQI=Linear(420.0,351.0,419.0,350.0,c); 
+ } 
+ else if (c>=420.0 && c<999.0) 
+ { 
+ AQI=Linear(500.0,421.0,999.0,420.0,c); 
+ } 
+ else 
+ { 
+ AQI="Out of Range"; 
+ } 
+ return AQIPM10IND; 
+ }
+
+
+function AQICategoryIND(AQIndex) 
+ { 
+ var AQI=parseFloat(AQIndex); 
+ var AQICategoryIND;           
+ if (AQI<=99.0) 
+ { 
+ AQICategoryIND="Good"; 
+ } 
+else if (AQI>101.0 && AQI<=150.0) 
+ { 
+ AQICategoryIND="Medium"; 
+ } 
+ else if (AQI>151.0 && AQI<=350.0) 
+ { 
+ AQICategoryIND="Poor"; 
+ } 
+ else if (AQI>351.0 && AQI<=420.0) 
+ { 
+ AQICategoryIND="Very Poor"; 
+ } 
+ else if (AQI>421.0 && AQI<=500.0) 
+ { 
+ AQICategoryIND="Severe"; 
+ } 
+ else 
+ { 
+ AQICategoryIND="Out of Range"; 
+ } 
+ return AQICategoryIND; 
+ }
+
+
+function AQIPM10US(Concentration) 
+ { 
+ var Conc=parseFloat(Concentration); 
+ var c; 
+ var AQI; 
+ c=Math.floor(Conc);           
+ if (c>=54.0 && c<0.0) 
+ { 
+ AQI=Linear(50.0,0.0,54.0,0.0,c); 
+ } 
+else if (c>=55.0 && c<154.0) 
+ { 
+ AQI=Linear(100.0,51.0,154.0,55.0,c); 
+ } 
+ else if (c>=155.0 && c<254.0) 
+ { 
+ AQI=Linear(150.0,101.0,254.0,155.0,c); 
+ } 
+ else if (c>=255.0 && c<354.0) 
+ { 
+ AQI=Linear(200.0,151.0,354.0,255.0,c); 
+ } 
+ else if (c>=355.0 && c<424.0) 
+ { 
+ AQI=Linear(300.0,201.0,424.0,355.0,c); 
+ } 
+ else if (c>=425.0 && c<504.0) 
+ { 
+ AQI=Linear(400.0,301.0,504.0,425.0,c); 
+ } 
+ else if (c>=505.0 && c<604.0) 
+ { 
+ AQI=Linear(500.0,401.0,604.0,505.0,c); 
+ } 
+ else 
+ { 
+ AQI="Out of Range"; 
+ } 
+ return AQIPM10US; 
+ }
+
+
+function AQICategoryUS(AQIndex) 
+ { 
+ var AQI=parseFloat(AQIndex); 
+ var AQICategoryUS;           
+ if (AQI<=54.0) 
+ { 
+ AQICategoryUS="Good"; 
+ } 
+else if (AQI>51.0 && AQI<=100.0) 
+ { 
+ AQICategoryUS="Moderate"; 
+ } 
+ else if (AQI>101.0 && AQI<=150.0) 
+ { 
+ AQICategoryUS="Unhealthy for sensitive Groups"; 
+ } 
+ else if (AQI>151.0 && AQI<=200.0) 
+ { 
+ AQICategoryUS="Unhealthy"; 
+ } 
+ else if (AQI>201.0 && AQI<=300.0) 
+ { 
+ AQICategoryUS="Very Unhealthy"; 
+ } 
+ else if (AQI>301.0 && AQI<=400.0) 
+ { 
+ AQICategoryUS="Hazardous"; 
+ } 
+ else if (AQI>401.0 && AQI<=500.0) 
+ { 
+ AQICategoryUS="Hazardous"; 
+ } 
+ else 
+ { 
+ AQICategoryUS="Out of Range"; 
+ } 
+ return AQICategoryUS; 
+ }
+
+
+function AQICalcPm10(form)
+{
+
+var f;
+f=document.form.inputbox.value
+f=f.replace(/\,/,'');
+
+var b1;
+
+document.form.inputbox.style.textAlign="center";
+document.form.inputbox.style.backgroundColor="white";
+b1=AQIPM10IND(f);	
+
+if (b1 == "Out of Range")
+{
+	alert("The concentration number you've entered is out of range for this pollutant.");
+	document.form.inputbox.value=" ";
+	document.form.outputbox1a.value=" ";
+	document.form.outputbox2a.value=" ";
+}
+
+else
+{
+	document.form.outputbox1a.value=b1;
+	document.form.outputbox2a.value=AQICategoryIND(b1);
+}
+document.form.outputbox1a.style.textAlign="center";
+document.form.outputbox2a.style.textAlign="center";
+
+if (document.form.outputbox2a.value == 'Good')
+{
+	document.form.outputbox3a.style.backgroundColor="#00b050";
+	document.form.outputbox3a.style.color="black";
+}
+else if (document.form.outputbox2a.value == 'Medium')
+{
+	document.form.outputbox3a.style.backgroundColor="#ffff00";
+	document.form.outputbox3a.style.color="black";
+}
+else if (document.form.outputbox2a.value == 'Poor')
+{
+	document.form.outputbox3a.style.backgroundColor="#00b0f0";
+	document.form.outputbox3a.style.color="black";
+}
+else if (document.form.outputbox2a.value == 'Very Poor')
+{
+	document.form.outputbox3a.style.backgroundColor="#0000cc";
+	document.form.outputbox3a.style.color="black";
+}
+else if (document.form.outputbox2a.value == 'Severe')
+{
+	document.form.outputbox3a.style.backgroundColor="#ff0000";
+	document.form.outputbox3a.style.color="black";
+}
+
+/////////
+////////
+//US
+var b13;
+
+document.form.inputbox.style.textAlign="center";
+document.form.inputbox.style.backgroundColor="white";
+b13=AQIPM10IND(f);	
+
+if (b13 == "Out of Range")
+{
+	alert("The concentration number you've entered is out of range for this pollutant.");
+	document.form.inputbox.value=" ";
+	document.form.outputbox1m.value=" ";
+	document.form.outputbox2m.value=" ";
+}
+
+else
+{
+	document.form.outputbox1m.value=b13;
+	document.form.outputbox2m.value=AQICategoryUS(b13);
+}
+document.form.outputbox1m.style.textAlign="center";
+document.form.outputbox2m.style.textAlign="center";
+
+if (document.form.outputbox2m.value == 'Good')
+{
+	document.form.outputbox3m.style.backgroundColor="#00e000";
+	document.form.outputbox3m.style.color="black";
+}
+else if (document.form.outputbox2m.value == 'Moderate')
+{
+	document.form.outputbox3m.style.backgroundColor="#ffff00";
+	document.form.outputbox3m.style.color="black";
+}
+else if (document.form.outputbox2m.value == 'Unhealthy for Sensitive Groups')
+{
+	document.form.outputbox3m.style.backgroundColor="#ff7600";
+	document.form.outputbox3m.style.color="black";
+}
+else if (document.form.outputbox2m.value == 'Unhealthy')
+{
+	document.form.outputbox3m.style.backgroundColor="#ff0000";
+	document.form.outputbox3m.style.color="black";
+}
+else if (document.form.outputbox2m.value == 'Very Unhealthy')
+{
+	document.form.outputbox3m.style.backgroundColor="#990049";
+	document.form.outputbox3m.style.color="#ffffff";
+}
+else if (document.form.outputbox2m.value == 'Hazardous')
+{
+	document.form.outputbox3m.style.backgroundColor="#7E0023";
+	document.form.outputbox3m.style.color="#ffffff";
+}
+else
+{
+	document.form.outputbox3m.style.backgroundColor="#ffffff";
+	document.form.outputbox3m.style.color="black";
+}
+
+return true;
 }
